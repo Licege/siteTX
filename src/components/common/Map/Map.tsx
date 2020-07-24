db6aa9bbb604c2a986d9444bf96a218c9a59afcc
Mark Turgunbaev <gotica-2@mail.ru>
2020-07-25 00:43:49 +0200

    dev

--- src/components/common/Map/Map.tsx
+++ src/components/common/Map/Map.tsx
@@ -1,5 +1,14 @@
 import React, {CSSProperties} from 'react'
-import {YMaps, Map, Placemark} from "react-yandex-maps";
+import {
+    YMaps,
+    Map,
+    Placemark,
+    FullscreenControl,
+    GeolocationControl,
+    TrafficControl,
+    TypeSelector,
+    ZoomControl
+} from "react-yandex-maps";
 import pointer from '../../../static/img/pointer-map.png'
 
 
@@ -11,7 +20,7 @@ interface IProps {
 //
 // }
 
-const CustomMap: React.FC<IProps> = ({ style }) => {
+const CustomMap: React.FC<IProps> = ({style}) => {
     let properties = {
         hintContent: '&lt;img src={pointer} /&gt;'
     }
@@ -24,14 +33,23 @@ const CustomMap: React.FC<IProps> = ({ style }) => {
         iconImageOffset: [-34, -76]
     }
 
-   // const style = {width: '100%', height: '424px'}
+    // const style = {width: '100%', height: '424px'}
 
     return (
         <YMaps style={style}>
-            <Map style={style} defaultState={{center: [54.649906, 20.366676], zoom: 17}}>
-                    <Placemark defaultGeometry={[54.649946, 20.366788]}
-                               properties={properties}
-                               options={pOptions} />
+            <Map style={style} defaultState={{
+                center: [54.649906, 20.366676],
+                controls: [],
+                zoom: 17
+            }}>
+                <Placemark defaultGeometry={[54.649946, 20.366788]}
+                           properties={properties}
+                           options={pOptions}/>
+                <FullscreenControl/>
+                <GeolocationControl/>
+                <TrafficControl/>
+                <ZoomControl/>
+                <TypeSelector/>
             </Map>
         </YMaps>
     )
