a928e3915d56aaf841576091cf172d87b7d1c3e5
licege <warraven53@gmail.com>
2020-12-06 18:16:36 +0200

    dev

--- src/components/common/elements/form/RenderTextField.tsx
+++ src/components/common/elements/form/RenderTextField.tsx
@@ -6,13 +6,17 @@ import './style.scss'
 interface IProps {
     label: string
     placeholder: string
-    input: any
+    input: {
+        name: string,
+        value: string,
+        onChange: () => void
+    }
     meta: {
         touched: boolean
         invalid: boolean
         error: string
     }
-    as?: 'textarea'|'input'
+    as?: 'textarea' | 'input'
     rows: any
     custom: any
 }
@@ -20,23 +24,23 @@ interface IProps {
 const renderTextField: React.FC<IProps> = ({
                                                label,
                                                placeholder,
-                                               input,
-                                               meta: {touched, invalid, error},
+                                               input: { name, value, onChange },
+                                               meta: { touched, invalid, error },
                                                as = 'input',
                                                ...custom
                                            }) => (
-        <div className={'field' + ((touched && invalid) ? ' error' : '')}>
-            {label ? <label className='field__label' htmlFor={input.name}>{label}</label> : null}
-            <FormControl id={input.name}
-                         as={as}
-                         onChange={input.onChange}
-                         defaultValue={input.value}
-                         isInvalid={touched && invalid}
-                         placeholder={placeholder}
-                         {...custom}
-            />
-            {touched && invalid && error ? <span className='text-field__prompt'>{error}</span> : null}
-        </div>
+    <div className={'field' + ((touched && invalid) ? ' error' : '')}>
+        {label ? <label className='field__label' htmlFor={name}>{label}</label> : null}
+        <FormControl id={name}
+                     as={as}
+                     onChange={onChange}
+                     defaultValue={value}
+                     isInvalid={touched && invalid}
+                     placeholder={placeholder}
+                     {...custom}
+        />
+        {touched && invalid && error ? <span className='text-field__prompt'>{error}</span> : null}
+    </div>
 )
 
 export default renderTextField
--- src/pages/complain/ComplainForm.tsx
+++ src/pages/complain/ComplainForm.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Field, InjectedFormProps, reduxForm } from 'redux-form'
 import Select from '../../components/common/elements/form/RenderSelect'
-import renderTextField from '../../components/common/elements/form/RenderTextField'
+import Datepicker from '../../components/common/elements/form/RenderDatepicker'
+import TextField from '../../components/common/elements/form/RenderTextField'
 import Button from '@material-ui/core/Button'
 
 const ComplainForm = () => {
     const complainType = ['Благодарность', 'Качество и вкус блюд', 'Неверный или неполный заказ', 'Обслуживание', 'Долгое ожидание', 'Вопрос по акции', 'Вопрос по доставке', 'Предложения', 'Другое']
-    const options = complainType.map((label, value) => ({ label, value }))
+    const options = complainType.map((label, value) => ({label, value}))
 
     const ComplainOptions = () => {
         return (
             <div>
                 <div>Тема обращения</div>
-                <Field name='type' component={Select} options={options} defaultValue={options[0]} />
+                <Field name='type' component={Select} options={options} defaultValue={options[0]}/>
             </div>
         )
     }
@@ -20,31 +21,14 @@ const ComplainForm = () => {
     return (
         <div className='complain-form__container'>
             <form className='complain-form'>
-                <Field name='type' component={Select} options={options} defaultValue={options[0]} label='Тема обращения' />
-                <div className='complain-form__field'>
-                    <Field name='name' component={renderTextField} label='Ваше имя' />
-                </div>
+                <Field name='type' component={Select} options={options} defaultValue={options[0]} label='Тема обращения'/>
+                <Field name='name' component={TextField} label='Ваше имя'/>
                 <div className='complain-form__row'>
-                    <div className='complain-form__field'>
-                        <Field name='email' component={renderTextField} label='Ваш e-mail' />
-                    </div>
-                    <div className='complain-form__field'>
-                        <Field name='phone' component={renderTextField} label='Ваш телефон' />
-                    </div>
+                    <Field name='email' component={TextField} label='Ваш e-mail'/>
+                    <Field name='phone' component={TextField} label='Ваш телефон'/>
                 </div>
-                <div className='complain-form__row'>
-                    <div className='complain-form__field'>
-                        <div>Дата посещения</div>
-                        <Field name='visitDate' component={renderTextField} />
-                    </div>
-                    <div className='complain-form__field'>
-                        <div>Время посещения</div>
-                        <Field name='visitTime' component={renderTextField} />
-                    </div>
-                </div>
-                <div>
-                    <Field name='text' component={renderTextField} label='Сообщение' as='textarea' rows={6} />
-                </div>
+                <Field name='visitDate' component={Datepicker} label='Дата и время посещения'/>
+                <Field name='text' component={TextField} label='Сообщение' as='textarea' rows={6}/>
                 <div className='complain-form__actions'>
                     <Button variant='contained' color='primary' type='submit'>Отправить</Button>
                 </div>
