a7504058afe8e4ad3ef0db310fe968ca3eb36330
licege <warraven53@gmail.com>
2021-07-17 10:53:05 +0200

    dev

--- dashboard/src/redux/getters/complain.getters.js
+++ dashboard/src/redux/getters/complain.getters.js
@@ -2,4 +2,4 @@ import { createDraftSafeSelector } from '@reduxjs/toolkit'
 
 const selector = state => state.complainsPage;
 
-export const getComplains = createDraftSafeSelector(selector, state => state.complainsPage)
+export const getComplains = createDraftSafeSelector(selector, state => state.complains)
