ea8249f2c98e827e0c9a0534edded838f7acf80f
licege <warraven53@gmail.com>
2021-10-16 13:17:31 +0200

    dev

--- server/src/servers/telegram.js
+++ server/src/servers/telegram.js
@@ -1,7 +1,9 @@
 const { Telegraf } = require('telegraf')
 
+// "lt --port 9093 --subdomain trixolma" for work
 const start = () => {
   if (!process.env.TELEGRAM_TOKEN) throw new Error('Set telegram token for use bot')
+  console.log(process.env.NODE_ENV);
 
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 
@@ -12,7 +14,7 @@ const start = () => {
   bot.launch({
     webhook: {
       domain: process.env.TELEGRAM_WEBHOOK_PATH,
-      port: 4000
+      port: process.env.TELEGRAM_BOT_PORT
     }
   })
 }
