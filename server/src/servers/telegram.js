bbdacddd212930a331f4781dfb0c856652d504d1
licege <warraven53@gmail.com>
2021-10-16 14:51:08 +0200

    dev

--- server/src/servers/telegram.js
+++ server/src/servers/telegram.js
@@ -1,11 +1,8 @@
 const { Telegraf } = require('telegraf')
-const fs = require('fs')
-const path = require('path')
 
 // "lt --port 9093 --subdomain trixolma" for work
 const start = () => {
   if (!process.env.TELEGRAM_TOKEN) throw new Error('Set telegram token for use bot')
-  console.log(process.env.NODE_ENV);
 
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 
@@ -13,25 +10,15 @@ const start = () => {
 
   bot.help((ctx) => ctx.reply("Справка в процессе"))
 
-  bot.launch({
-    webhook: {
-      domain: process.env.TELEGRAM_WEBHOOK_PATH,
-      port: process.env.TELEGRAM_BOT_PORT,
-      tlsOptions: getTLS()
-    }
-  })
-}
-
-const pathToCert = path.resolve(__dirname, '../../certificates/')
-
-function getTLS() {
-  if (process.env.NODE_ENV !== 'production') return undefined;
-
-  return undefined;
-
-  return {
-    key: fs.readlinkSync(path.resolve(pathToCert, 'privkey.pem')),
-    cert: fs.readFileSync(path.resolve(pathToCert, 'cert.pem'))
+  if (process.env.NODE_ENV === 'development') {
+    bot.launch({
+      webhook: {
+        domain: process.env.TELEGRAM_WEBHOOK_PATH,
+        port: process.env.TELEGRAM_BOT_PORT
+      }
+    })
+  } else {
+    bot.launch()
   }
 }
 
