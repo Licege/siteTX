b9c70505f0dca62abbe85abca99b2a0f85c4347a
licege <warraven53@gmail.com>
2022-03-08 12:38:00 +0200

    Add craco for defer non-critical css

---
+++ site/craco.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { whenProd } = require('@craco/craco');
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin');
+
+module.exports = {
+  webpack: {
+    configure: webpackConfig => ({
+      ...webpackConfig,
+      plugins: [
+        ...webpackConfig.plugins,
+        ...whenProd(() => [
+          new HtmlCriticalWebpackPlugin({
+            base: path.resolve(__dirname, 'build'),
+            src: 'index.html',
+            dest: 'index.html',
+            inline: true,
+            minify: true,
+            extract: true,
+            width: 320,
+            height: 565,
+            penthouse: {
+              blockJSRequests: false,
+            }
+          })
+        ], [])
+      ]
+    })
+  }
+}
