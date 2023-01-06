b6b9bd703e88db4e96fb96050c144e038f6ee656
licege <warraven53@gmail.com>
2023-01-06 11:27:47 +0200

    Fix leak memory

--- site/src/hooks/windowResize.ts
+++ site/src/hooks/windowResize.ts
@@ -3,15 +3,17 @@ import { useEffect, useState } from 'react';
 export const useWindowSize = () => {
   const [size, setSize] = useState({ width: window.innerWidth, height: window.innerHeight })
 
-  const handleResize = () => {
-    setSize({ width: window.innerWidth, height: window.innerHeight })
-  }
-
   useEffect(() => {
+    const handleResize = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight })
+    }
+
     window.addEventListener('resize', handleResize)
 
-    return () => window.removeEventListener('resize', handleResize)
-  }, [size])
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return size
 }
