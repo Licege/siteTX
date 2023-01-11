fd340bba90618b299e19cd61de8db435a8397d21
licege <warraven53@gmail.com>
2023-01-11 21:10:45 +0200

    [ts-migrate][.] Rename files from JS/JSX to TS/TSX
    Co-authored-by: ts-migrate <>

---
+++ server/config/keys.ts
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURL:
+    'mongodb+srv://root-licege:s5SshnqjXv1PWup7@cluster0-wz9ft.mongodb.net/test?retryWrites=true&w=majority',
+  jwt: 'dev-jwtfsfsdfewt2ggegreyerbvnhjryjbcbbcvbggergernvgrntr'
+}
