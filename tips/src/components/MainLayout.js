13bf76dcec7256f3b9c470c079a5057b6b9095cc
licege <warraven53@gmail.com>
2026-06-06 22:00:03 +0200

    feat: изменение логотипа

--- tips/src/components/MainLayout.js
+++ tips/src/components/MainLayout.js
@@ -12,8 +12,9 @@ export default function MainLayout({ title = 'Три холма', children }) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="48x48" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta charSet="utf-8" />
         <title>{title}</title>
       </Head>
@@ -21,7 +22,7 @@ export default function MainLayout({ title = 'Три холма', children }) {
         <header className={classes.header}>
           <div className={classes.header__logo}>
             <a href="https://tri-xolma.ru/" target="_blank" rel="noreferrer">
-              <Image src={logo} width={111} height={64} alt="Три Холма" />
+              <Image src={logo} width={73} height={64} alt="Три Холма" />
             </a>
           </div>
         </header>
